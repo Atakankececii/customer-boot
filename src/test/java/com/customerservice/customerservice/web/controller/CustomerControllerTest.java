84de833b092f5f81cdca981c71a4f90cbbd34ff3
Atakankececii <atakankececi@hotmail.com>
2025-02-15 20:23:35 +0300

    Deneme

--- src/test/java/com/customerservice/customerservice/web/controller/CustomerControllerTest.java
+++ src/test/java/com/customerservice/customerservice/web/controller/CustomerControllerTest.java
@@ -1,21 +1,15 @@
 package com.customerservice.customerservice.web.controller;
 
 import com.customerservice.customerservice.web.model.CustomerDto;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import javax.print.attribute.standard.Media;
-import java.lang.runtime.ObjectMethods;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
